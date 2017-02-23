bd515413a8fa517fb0d3e48515da28912df1e105
maxikg <maxmax58@googlemail.com>
2017-02-23 01:24:11 +0100

    Updated deployment script

--- .build/scripts/deploy-github.sh
+++ .build/scripts/deploy-github.sh
@@ -8,6 +8,6 @@ git config --global user.name "LunaMC Auto Deployment"
 git config --global user.email max.walsch@gmail.com
 
 # Git add, commit and push
-git add target/* || exit 0
+git add --all || exit 0
 git commit -m "Update data" || exit 0
 git push origin master || exit 0
