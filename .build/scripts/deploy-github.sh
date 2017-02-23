ab3bc622dbad0759b41ac8150f072bda3b3773ab
maxikg <maxmax58@googlemail.com>
2017-02-23 01:38:31 +0100

    Updated deployment script

--- .build/scripts/deploy-github.sh
+++ .build/scripts/deploy-github.sh
@@ -1,13 +1,17 @@
 #!/usr/bin/env bash
 
 # Decrypt private push key
+mkdir -p ~/.ssh
 openssl aes-256-cbc -K $encrypted_24624dd287c1_key -iv $encrypted_24624dd287c1_iv -in .build/resources/id_rsa.enc -out ~/.ssh/id_rsa -d
 
 # First time git setup
 git config --global user.name "LunaMC Auto Deployment"
 git config --global user.email max.walsch@gmail.com
 
+# Setup deployment remote
+git remote add deployment sshgit@github.com:LunaMC/mc-meta.git
+
 # Git add, commit and push
 git add --all || exit 0
 git commit -m "Update data [ci skip]" || exit 0
-git push origin master || exit 0
+git push deployment master || exit 0
